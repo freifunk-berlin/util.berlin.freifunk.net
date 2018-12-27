d4d4208c29c5efdab852d0cd3f782b63337ff02c
sarumpaet <sarumpaet@users.noreply.github.com>
2018-12-27 16:54:26 +0100

    update_node: keep local copy of OWM data for owm2ffmap

--- www/update_node.php
+++ www/update_node.php
@@ -1,9 +1,14 @@
 <?php
 $nodename = $_SERVER["REQUEST_URI"];
 $nodename = preg_replace("/^\/update_node\//", "", $nodename);
+if(strlen($nodename) > 50) die("Node name too long");
 
 $entityBody = file_get_contents('php://input');
 $entityBody = preg_replace("/\"mac\"\:\"..\:..\:XX\:XX\:..\:..\"/", "\"mac\":\"redacted\"", $entityBody);
+
+$filename = "/var/opt/ffmapdata/" . str_replace(".", "%2E", urlencode($nodename)) . ".json";
+file_put_contents($filename, $entityBody, LOCK_EX);
+
 $options = array(
   'http' => array(
     'method'  => 'PUT',
