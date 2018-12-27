13f8eb56bff439ff30994dae120d1812e2d852ef
sarumpaet <sarumpaet@users.noreply.github.com>
2018-12-27 20:12:07 +0100

    update_node: keep ctime

--- www/update_node.php
+++ www/update_node.php
@@ -6,8 +6,12 @@ if(strlen($nodename) > 50) die("Node name too long");
 $entityBody = file_get_contents('php://input');
 $entityBody = preg_replace("/\"mac\"\:\"..\:..\:XX\:XX\:..\:..\"/", "\"mac\":\"redacted\"", $entityBody);
 
-$filename = "/var/opt/ffmapdata/" . str_replace(".", "%2E", urlencode($nodename)) . ".json";
-file_put_contents($filename, $entityBody, LOCK_EX);
+$filename = "/var/opt/ffmapdata/" . str_replace(".", "%2E", urlencode($nodename));
+file_put_contents($filename . ".json_new", $entityBody);
+rename($filename . ".json_new", $filename . ".json");
+if(!is_file($filename . ".ctime")) {
+  touch($filename . ".ctime");
+}
 
 $options = array(
   'http' => array(
