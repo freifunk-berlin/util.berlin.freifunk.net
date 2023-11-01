8a584f67aaabae081502ab1c10489858f610ff3f
noxilixon <93288913+noxilixon@users.noreply.github.com>
2023-11-01 23:04:55 +0100

    Added path based redirect (#19)

--- www/knoteninfo.php
+++ www/knoteninfo.php
@@ -1,14 +1,56 @@
 <?php
 
-// https://util.berlin.freifunk.net/knoteninfo?knoten=Zwingli-Nord-2GHz&typ=wiki
+// Query based redirect
+// Example: https://util.berlin.freifunk.net/knoteninfo?knoten=Zwingli-Nord-2GHz&typ=wiki
 // typ: wiki, monitor, owm, hopglass
+//
+// Path based redirect
+// Example: https://ff.berlin/d/linie206-core
+// /d/ -> documentation (wiki)
+// /m/ -> map (hopglass)
+// /s/ -> statistics (monitor)
 
-$knoten = ($_GET["knoten"] ?? "%");
+$path_elements = getPathElements($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']);
+
+if (count($_GET)) {
+  $knoten = ($_GET["knoten"] ?? "%");
+  $typ = ($_GET["typ"] ?? "");
+} else if (count($path_elements) == 2) {
+  switch ($path_elements[0]) {
+    case "d":
+      $typ = "wiki";
+      break;
+    case "m":
+      $typ = "hopglass";
+      break;
+    case "s":
+      $typ = "monitor";
+      break;
+  }
+  ;
+  $knoten = $path_elements[1];
+} else {
+  die("Keine gültige Anfrage.");
+}
+
 $knoten = preg_replace("/\.olsr$/", "", $knoten);
 
 if(preg_match('/[^A-Za-z0-9\\.\\-\\_]/', $knoten)) die("Ungültiger Knotenname.");
 
-$typ = ($_GET["typ"] ?? "");
+
+function getPathElements($request_uri, $script_name) {
+  
+  if (strpos($request_uri, $script_name) === 0) {
+    $request_path = substr($request_uri, strlen($script_name));
+  } else {
+    $request_path = $request_uri;
+  }
+  
+  $parsed_url = parse_url($request_path);
+  $path = $parsed_url['path'];
+  return explode('/', trim($path, '/'));
+}
+
 
 function getUrl($url) {
   $ctx = stream_context_create(["http" => ["method" => "GET"]]);
@@ -18,7 +60,8 @@ function getUrl($url) {
   return $res;
 }
 
-function getWikiLink($knoten) {
+
+function getWikiLink($knoten){
   $url = "https://wiki.freifunk.net/api.php?action=ask&query=[[Hat_Knoten%3A%3A".$knoten."]]|%3FModification%20date|sort%3DModification%20date|order%3Ddesc&format=json";
   $body = getUrl($url);
   $json = json_decode($body);
@@ -56,11 +99,11 @@ if($typ === "wiki") {
 
 echo "<i>$knoten</i> auf...";
 echo "<ul>".
-     "<li><a href=\"$owmurl\">openwifimap.net</a></li>".
-     "<li><a href=\"$hgurl\">hopglass.berlin.freifunk.net</a></li>".
-     "<li><a href=\"/knoteninfo?knoten=$knoten&typ=wiki\">wiki.freifunk.net</a></li>".
-     "<li><a href=\"$monurl\">monitor.berlin.freifunk.net</a></li>".
-     "</ul>".
-     "zu <a href=\"https://berlin.freifunk.net/\">berlin.freifunk.net</a></body>";
+  "<li><a href=\"$owmurl\">openwifimap.net</a></li>".
+  "<li><a href=\"$hgurl\">hopglass.berlin.freifunk.net</a></li>".
+  "<li><a href=\"/knoteninfo?knoten=$knoten&typ=wiki\">wiki.freifunk.net</a></li>".
+  "<li><a href=\"$monurl\">monitor.berlin.freifunk.net</a></li>".
+  "</ul>".
+  "zu <a href=\"https://berlin.freifunk.net/\">berlin.freifunk.net</a></body>";
 
 ?>
